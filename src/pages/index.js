c5610b370e6bb47420959f6adf3c184c45b2b980
yoso4337@gmail.com <yoso4337@gmail.com>
2023-04-19 03:35:08 +0000

    cc

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ export default function IndexPage() {
     <ThemeProvider theme={theme}>
       <StickyProvider>
         <Layout>
-          <SEO title="Startup Landing 005" />
+          <SEO title="Multicls" />
           <Banner />
           <KeyFeature />
           <ServiceSection />
